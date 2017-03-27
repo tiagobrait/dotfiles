c33c612eedb8f3b05776b7abe2d97c9f1a46d676
Tiago Brait <tiagobrait@gmail.com>
2017-03-27 16:55:44 -0300

    add golang version manager

---
+++ gvm-env.sh
@@ -0,0 +1,7 @@
+#nvm for everybody who uses it
+#2017 tiagobrait
+
+GVM_DIR="$HOME/.gvm"
+
+[[ -s "$GVM_DIR/scripts/gvm" ]] && source "$GVM_DIR/scripts/gvm"
+[[ -r "$GVM_DIR/scripts/completion" ]] && source "$GVM_DIR/scripts/completion"
